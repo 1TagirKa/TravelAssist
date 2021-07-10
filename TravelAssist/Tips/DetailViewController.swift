9b15a44c91d28a155c34a8a284e68e3750920f4d
Алексей Горбунов <nayfront@MacBook-Air-Aleksej.local>
2021-07-11 01:22:05 +0300

    Task completed successfully

--- TravelAssist/Tips/DetailViewController.swift
+++ TravelAssist/Tips/DetailViewController.swift
@@ -14,30 +14,29 @@ class DetailViewController: UIViewController {
     var article: Article!
     
     @IBOutlet weak var articleImage: UIImageView!
-    @IBOutlet weak var articleText: UITextView!
+    @IBOutlet weak var articleText: UILabel!
     @IBOutlet weak var articleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         articleLabel.text = article.articleLabel
+        articleLabel.layer.cornerRadius = 15
+        articleLabel.layer.borderWidth = 3
+        articleLabel.layer.borderColor = UIColor.systemGray6.cgColor
+        
         
         articleImage.image = article.articleImage
         articleImage.layer.cornerRadius = 15
         articleImage.contentMode = .scaleAspectFill
         
         articleText.text = article.articleText
-        articleText.layer.cornerRadius = 15
-        articleText.backgroundColor = .systemGray6
-        articleText.isEditable = false
+//        articleText.layer.cornerRadius = 15
+//        articleText.layer.borderWidth = 3
+//        articleText.layer.borderColor = UIColor.systemGray6.cgColor
         
         articleText.translatesAutoresizingMaskIntoConstraints = true
         articleText.sizeToFit()
-        
-        if articleText.frame.size.height > 310 {
-            articleText.frame.size.height = 310
-            articleText.isScrollEnabled = true
-        }
     }
     
 }
